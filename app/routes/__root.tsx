ca12e1ec1085d6ea3443ab7f96ea2d8de9d258e7
Japheth Kiptoo <thygon6422@gmail.com>
2025-02-23 18:40:11 +0300

    Add: index router - setup root route

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,1 +1,30 @@
+import { HeadContent, Scripts } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import type { ReactNode } from "react";
+
 //application root
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
